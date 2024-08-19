d870d91c8dfd4163b83ad8c818954418428c8ec3
dikiamust <agungmustaqimdiki@gmail.com>
2024-08-20 00:09:41 +0700

    feat: add seeder command on docker-compose

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,26 +4,34 @@ import * as bcrypt from 'bcrypt';
 const prisma = new PrismaClient();
 
 async function main() {
-  const salt = await bcrypt.genSalt();
-  const hash = await bcrypt.hash('Aa!45678', salt);
-  // User as system
-  const user = await prisma.user.create({
-    data: {
-      name: 'system',
+  const isUserAsSystemAlreadyExist = await prisma.user.findFirst({
+    where: {
       email: 'system@mail.com',
-      password: hash,
-      salt,
     },
   });
 
-  const systemPaymentAccount = await prisma.paymentAccount.create({
-    data: {
-      userId: user.id,
-      balance: 1000000,
-      type: AccountType.DEBIT,
-      currency: Currency.USD,
-    },
-  });
+  if (!isUserAsSystemAlreadyExist) {
+    const salt = await bcrypt.genSalt();
+    const hash = await bcrypt.hash('Aa!45678', salt);
+    // User as system
+    const user = await prisma.user.create({
+      data: {
+        name: 'system',
+        email: 'system@mail.com',
+        password: hash,
+        salt,
+      },
+    });
+
+    const systemPaymentAccount = await prisma.paymentAccount.create({
+      data: {
+        userId: user.id,
+        balance: 1000000,
+        type: AccountType.DEBIT,
+        currency: Currency.USD,
+      },
+    });
+  }
 }
 
 main()
